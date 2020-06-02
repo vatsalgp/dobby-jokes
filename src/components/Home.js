9888762ac1f163bd40b2bb347697782be66d4832
Vatsal Gupta <vatsalgupta280@gmail.com>
2020-06-02 15:08:41 +0530

    changed spacing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,20 @@
 import React from "react";
-import { Card, Container } from "semantic-ui-react";
-import { Link } from "react-router-dom";
+import { Card } from "semantic-ui-react";
 import jokes from "../jokes";
 
 const Home = () => {
 	const renderedJokes = jokes.map(([ header ], id) => (
-		<Link to={"/" + id} key={id}>
-			<Card fluid header={header} color="blue" as="h3" style={{ marginTop: "30px" }} />
-		</Link>
+		<Card
+			fluid
+			header={header}
+			href={"/" + id}
+			key={id}
+			color="blue"
+			style={{ marginTop: "30px", marginBottom: "0" }}
+		/>
 	));
 
-	return (
-		<Card.Group>
-			<Container>{renderedJokes}</Container>
-		</Card.Group>
-	);
+	return <Card.Group>{renderedJokes}</Card.Group>;
 };
 
 export default Home;
--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,22 +1,13 @@
 import React from "react";
 import jokes from "../jokes";
-import { Card, Container } from "semantic-ui-react";
+import { Card } from "semantic-ui-react";
 
 const Joke = props => {
 	const { id } = props.match.params;
 	const [ header, description ] = jokes[id];
 	return (
 		<Card.Group>
-			<Container>
-				<Card
-					fluid
-					color="blue"
-					as="h3"
-					style={{ marginTop: "30px" }}
-					header={header}
-					description={description}
-				/>
-			</Container>
+			<Card fluid color="blue" style={{ marginTop: "30px" }} header={header} description={description} />
 		</Card.Group>
 	);
 };
