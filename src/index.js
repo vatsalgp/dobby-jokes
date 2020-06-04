f4a88bc42f5ce68e7053f344ccf3610b8f3f9b92
Vatsal Gupta <vatsalgupta280@gmail.com>
2020-06-05 02:29:53 +0530

    load css at end of the page

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App";
 import { register } from "./serviceWorker";
-import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
