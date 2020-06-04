1695ef515b72ff510f223e07a41616b96e4f44db
Vatsal Gupta <vatsalgupta280@gmail.com>
2020-06-05 02:43:24 +0530

    Revert "load css at end of the page"
    This reverts commit f4a88bc42f5ce68e7053f344ccf3610b8f3f9b92.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App";
 import { register } from "./serviceWorker";
+import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
